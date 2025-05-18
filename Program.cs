cadcf9e3887cfe82c85e59ea3bf9618207848ab6
Kawan Weber <kawan_weber@hotmail.com>
2025-05-18 11:23:42 -0300

    MemeEngenharia

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Está se movendo? (s/n)");
+string movendo = Console.ReadLine()!;
+
+if (movendo == "s")
+{
+    Console.WriteLine("Deveria? (s/n)");
+    string deveria = Console.ReadLine()!;
+
+    if (deveria == "s")
+    {
+        Console.WriteLine("Ótimo.");
+    }
+    else
+    {
+        Console.WriteLine("Use fita adesiva.");
+    }
+}
+else
+{
+    Console.WriteLine("Deveria? (s/n)");
+    string deveria = Console.ReadLine()!;
+
+    if (deveria == "s")
+    {
+        Console.WriteLine("Use WD-40.");
+    }
+    else
+    {
+        Console.WriteLine("Ótimo.");
+    }
+}
